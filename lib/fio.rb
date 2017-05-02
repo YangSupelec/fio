f3ea0a1392f2e916cfa990a3da5d50f0f7635ff9
yang zhang <yang@ya.zhang>
2017-05-02 12:15:28 +0200

    Include Module Outside the Method

--- lib/fio.rb
+++ lib/fio.rb
@@ -2,14 +2,14 @@ require "fio/version"
 require "fio/install"
 
 module Fio
+	include Install
 	def self.benchmark
 		
 		if File.file?("/usr/local/bin/fio")
 			output = `fio -v`
 			puts "#{output} has been already installed"
 		else
-			include Install 
-			Install.install
+			 install
 		end
 		
 	end
