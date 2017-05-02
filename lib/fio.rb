1ba1493fe8e6e6bf29d935657266a3dda5264763
yang zhang <yang@ya.zhang>
2017-05-02 11:06:25 +0200

    Seperate Install From the Main Program

--- lib/fio.rb
+++ lib/fio.rb
@@ -1,34 +1,17 @@
 require "fio/version"
+require "fio/install"
 
 module Fio
-	INSTALL_DIR = '~/opt/fio'
-	FIO_VERSION = '2.19'
-	def benchmark
-		# Update ubuntu package manager
-		system "sudo apt-get update"
-
-		# Install base package
-		system "sudo apt-get install build-essential libaio-dev -y"
+	def self.benchmark
+		output = `fio -v`
 		if $?.exitstatus > 0
-  			puts "Failed to install basic package." 
-		end
-
-		# Create installation directory if missed
-		unless File.directory?(INSTALL_DIR)
-			system "mkdir -p #{INSTALL_DIR}"
-		end
-
-		# Install fio from source code
-		if $?.exitstatus == 0
-			system "cd #{INSTALL_DIR} && wget http://brick.kernel.dk/snaps/fio-#{FIO_VERSION}.tar.bz2 && \
-					tar -xjvf fio-#{FIO_VERSION}.tar.bz2 && \
-					cd fio-#{FIO_VERSION} && ./configure && make && sudo make install"
-			# Check install version
-			if $?.exitstatus == 0
-				output = `fio -v`
-				puts output
-			end
+			include Install 
+			install
+		else
+			puts "#{output} has been already installed"
 		end
 		
 	end
 end
+
+benchmark
