1a0c1104b70f6e88b28f84064b3f25d1b76bcea7
yang zhang <yang@ya.zhang>
2017-05-02 13:49:42 +0200

    Refactor Structure

--- lib/fio.rb
+++ lib/fio.rb
@@ -1,17 +1,47 @@
 require "fio/version"
 require "fio/install"
 
-module Fio
-	include Install
+class Fio
+	INSTALL_DIR = '~/opt/fio'
+	FIO_VERSION = '2.19'
+
 	def self.benchmark
 		
 		if File.file?("/usr/local/bin/fio")
 			output = `fio -v`
 			puts "#{output} has been already installed"
 		else
-			 self.install
+			install
 		end
 		
+	end
+
+	def install
+		# Update ubuntu package manager
+		system "sudo apt-get update"
+
+		# Install base package
+		system "sudo apt-get install build-essential libaio-dev -y"
+		if $?.exitstatus > 0
+  			puts "Failed to install basic package." 
+		end
+
+		# Create installation directory if missed
+		unless File.directory?(INSTALL_DIR)
+			system "mkdir -p #{INSTALL_DIR}"
+		end
+
+		# Install fio from source code
+		if $?.exitstatus == 0
+			system "cd #{INSTALL_DIR} && wget http://brick.kernel.dk/snaps/fio-#{FIO_VERSION}.tar.bz2 && \
+					tar -xjvf fio-#{FIO_VERSION}.tar.bz2 && \
+					cd fio-#{FIO_VERSION} && ./configure && make && sudo make install"
+			# Check install version
+			if $?.exitstatus == 0
+				output = `fio -v`
+				puts output
+			end
+		end
 	end
 end
 
